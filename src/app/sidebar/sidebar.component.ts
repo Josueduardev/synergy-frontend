954fffa31ba3c7899f0cfe3f59c1c656b04e8ecd
AdoDeveloper <105835711+AdoDeveloper@users.noreply.github.com>
2024-11-19 02:52:12 -0600

    Se agregaron los componentes de sidebar y navbar en el layout para la version 17.3.11 de angular con el paquete UI de primeng en la version 20.9.0 de node js

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+})
+export class SidebarComponent {
+  @Input() visible: boolean = true; // Recibe el estado del Sidebar desde el componente principal
+}
