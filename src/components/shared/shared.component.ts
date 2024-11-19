b6e7e45f1e9b8f0976d169e00ca6e788b8d0754c
Alexander Chinque <alexchinke97@gmail.com>
2024-11-19 17:15:47 -0600

    Reestructurando la plantilla por separacion de login

---
+++ src/components/shared/shared.component.ts
@@ -0,0 +1,21 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], // Add this if needed
+  imports: [CommonModule, RouterOutlet, SidebarComponent, NavbarComponent],
+  templateUrl: './shared.component.html',
+  styleUrls: ['./shared.component.scss'],
+})
+export class SharedComponent {
+  public sidebarVisible: boolean = true;
+
+  toggleSidebar() {
+    this.sidebarVisible = !this.sidebarVisible;
+  }
+}
