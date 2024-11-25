9ad872d52967ef4f892b82d0ad92484c7a591248
AdoDeveloper <105835711+AdoDeveloper@users.noreply.github.com>
2024-11-25 11:01:42 -0600

    Solicitudes page

--- src/components/filtros/filtros.component.ts
+++ src/components/filtros/filtros.component.ts
@@ -2,36 +2,62 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CalendarModule } from 'primeng/calendar';
-import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { FiltrosButtonComponent } from './filtros-button/filtros-button.component';
+import { DesembolsarButtonComponent } from './desembolsar-button/desembolsar-button.component';
+import { ModalFiltrosAvanzadosComponent } from './modal-filtros-avanzados/modal-filtros-avanzados.component';
 
 @Component({
   selector: 'app-filtros',
   standalone: true,
-  imports: [CommonModule, FormsModule, CalendarModule, ButtonModule, InputTextModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    CalendarModule,
+    InputTextModule,
+    ButtonModule,
+    FiltrosButtonComponent,
+    DesembolsarButtonComponent,
+    ModalFiltrosAvanzadosComponent,
+  ],
   templateUrl: './filtros.component.html',
   styleUrls: ['./filtros.component.scss'],
 })
 export class FiltrosComponent {
-  searchText: string = ''; // Texto de búsqueda
-  rangoFechas: Date[] = []; // Rango de fechas
+  searchText: string = ''; // Texto del input de búsqueda
+  rangeDates: Date[] | null = null; // Rango de fechas seleccionado
+  placeholder: string = 'Seleccionar rango de fechas'; // Texto del placeholder
+  mostrarModalFiltros: boolean = false; // Controla la visibilidad del modal
 
-  filtrarPorRango() {
-    if (this.rangoFechas.length === 2) {
-      const [fechaInicio, fechaFin] = this.rangoFechas;
-      console.log(
-        `Filtrando desde ${fechaInicio.toLocaleDateString('es-ES')} hasta ${fechaFin.toLocaleDateString('es-ES')}`
-      );
+  actualizarRangoTexto() {
+    if (this.rangeDates && this.rangeDates[0] && this.rangeDates[1]) {
+      const options: Intl.DateTimeFormatOptions = { month: 'long', day: 'numeric' };
+      const formatter = new Intl.DateTimeFormat('es-ES', options);
+      this.placeholder = `${formatter.format(this.rangeDates[0])} - ${formatter.format(this.rangeDates[1])}`;
     } else {
-      console.log('Selecciona un rango de fechas válido.');
+      this.placeholder = 'Seleccionar rango de fechas';
     }
   }
 
-  desembolsar() {
-    console.log('Acción de desembolsar ejecutada.');
+  clearRangoTexto() {
+    this.rangeDates = null;
+    this.placeholder = 'Seleccionar rango de fechas';
+  }
+
+  aplicarSeleccion() {
+    console.log('Rango de fechas seleccionado:', this.placeholder);
   }
 
   buscar() {
-    console.log(`Buscando: ${this.searchText}`);
+    console.log('Buscando:', this.searchText);
+  }
+
+  mostrarModal() {
+    this.mostrarModalFiltros = true; // Muestra el modal
+  }
+
+  cerrarModal() {
+    this.mostrarModalFiltros = false; // Cierra el modal
   }
 }
--- src/components/tabla-solicitudes/tabla-solicitudes.component.ts
+++ src/components/tabla-solicitudes/tabla-solicitudes.component.ts
@@ -1,57 +1,94 @@
 import { Component, OnInit } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table';
-import { PaginatorModule } from 'primeng/paginator';
 import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { PaginatorModule } from 'primeng/paginator';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-tabla-solicitudes',
   standalone: true,
-  imports: [CommonModule, ButtonModule, TableModule, PaginatorModule],
+  imports: [CommonModule,
+    TableModule,
+    ButtonModule,
+    PaginatorModule,
+    InputTextModule,
+],
   templateUrl: './tabla-solicitudes.component.html',
-  styleUrls: ['./tabla-solicitudes.component.scss']
+  styleUrls: ['./tabla-solicitudes.component.scss'],
 })
 export class TablaSolicitudesComponent implements OnInit {
   solicitudes: any[] = [];
-  cols: any[] = []; // Inicializamos cols como un arreglo vacío
-  totalRecords: number = 100; // Simular total de registros
+  cols: any[] = [];
+  totalRecords: number = 30; // Total de registros
   rows: number = 10; // Filas por página
-  first: number = 0; // Paginación inicial
 
   constructor() {}
 
-  ngOnInit() {
-    // Column definitions
+  ngOnInit(): void {
+    // Definición de columnas con tipos de filtros
     this.cols = [
-      { field: 'cliente', header: 'Cliente' },
-      { field: 'correo', header: 'Correo' },
-      { field: 'nrc', header: 'NRC Emisor' },
-      { field: 'encargado', header: 'Encargado' },
-      { field: 'telefono', header: 'Teléfono' },
-      { field: 'monto', header: 'Monto' },
-      { field: 'interes', header: 'Interés' },
-      { field: 'acciones', header: 'Acciones' }
+      { field: 'cliente', header: 'Cliente', filterType: 'text' },
+      { field: 'correo', header: 'Correo', filterType: 'text' },
+      { field: 'nrc', header: 'NRC Emisor', filterType: 'text' },
+      { field: 'encargado', header: 'Encargado', filterType: 'text' },
+      { field: 'telefono', header: 'Teléfono', filterType: 'text' },
+      { field: 'monto', header: 'Monto', filterType: 'numeric' },
+      { field: 'interes', header: 'Interés', filterType: 'numeric' },
     ];
-
-    // Simulated data
-    this.solicitudes = Array.from({ length: this.totalRecords }, (_, index) => ({
-      cliente: `Nombre Completo Del Cliente ${index + 1}`,
-      correo: `cliente${index + 1}@example.com`,
-      nrc: `${Math.floor(Math.random() * 99999999)}`,
-      encargado: `Nombre Completo Del Encargado ${index + 1}`,
-      telefono: `123-456-${index.toString().padStart(4, '0')}`,
-      monto: Math.floor(Math.random() * 10000),
-      interes: `${Math.floor(Math.random() * 10)}%`
-    }));
+    // Registros quemados
+    this.solicitudes = [
+      { cliente: 'Juan Pérez', correo: 'juan.perez@example.com', nrc: '12345', encargado: 'Ana López', telefono: '+503 1234-5678', monto: 1000, interes: '5%' },
+      { cliente: 'María López', correo: 'maria.lopez@example.com', nrc: '23456', encargado: 'Luis Gómez', telefono: '+503 5678-1234', monto: 2000, interes: '3%' },
+      { cliente: 'Carlos Hernández', correo: 'carlos.hernandez@example.com', nrc: '34567', encargado: 'Pedro Sánchez', telefono: '+503 9876-5432', monto: 3000, interes: '4%' },
+      { cliente: 'Ana González', correo: 'ana.gonzalez@example.com', nrc: '45678', encargado: 'Carmen Ruiz', telefono: '+503 3456-7890', monto: 4000, interes: '6%' },
+      { cliente: 'Luis Martínez', correo: 'luis.martinez@example.com', nrc: '56789', encargado: 'Raúl Pérez', telefono: '+503 4321-0987', monto: 5000, interes: '7%' },
+      { cliente: 'Pedro Gutiérrez', correo: 'pedro.gutierrez@example.com', nrc: '67890', encargado: 'Laura Torres', telefono: '+503 7654-3210', monto: 6000, interes: '2%' },
+      { cliente: 'Laura Torres', correo: 'laura.torres@example.com', nrc: '78901', encargado: 'Carlos Ramírez', telefono: '+503 8765-4321', monto: 7000, interes: '8%' },
+      { cliente: 'Raúl Ramírez', correo: 'raul.ramirez@example.com', nrc: '89012', encargado: 'Sofía Castro', telefono: '+503 6543-2109', monto: 8000, interes: '9%' },
+      { cliente: 'Sofía Castro', correo: 'sofia.castro@example.com', nrc: '90123', encargado: 'María León', telefono: '+503 5432-1098', monto: 9000, interes: '1%' },
+      { cliente: 'María León', correo: 'maria.leon@example.com', nrc: '01234', encargado: 'Jorge Fernández', telefono: '+503 2109-8765', monto: 10000, interes: '5%' },
+      { cliente: 'Ricardo Álvarez', correo: 'ricardo.alvarez@example.com', nrc: '11234', encargado: 'Ana Morales', telefono: '+503 5678-2345', monto: 11000, interes: '6%' },
+      { cliente: 'Ana Morales', correo: 'ana.morales@example.com', nrc: '22345', encargado: 'Luis Torres', telefono: '+503 8765-1234', monto: 12000, interes: '7%' },
+      { cliente: 'Luis Torres', correo: 'luis.torres@example.com', nrc: '33456', encargado: 'Carmen Gutiérrez', telefono: '+503 5432-6789', monto: 13000, interes: '4%' },
+      { cliente: 'Carmen Gutiérrez', correo: 'carmen.gutierrez@example.com', nrc: '44567', encargado: 'Juan Ramírez', telefono: '+503 2109-5432', monto: 14000, interes: '3%' },
+      { cliente: 'Juan Pérez', correo: 'juan.perez@example.com', nrc: '12345', encargado: 'Ana López', telefono: '+503 1234-5678', monto: 1000, interes: '5%' },
+      { cliente: 'María López', correo: 'maria.lopez@example.com', nrc: '23456', encargado: 'Luis Gómez', telefono: '+503 5678-1234', monto: 2000, interes: '3%' },
+      { cliente: 'Carlos Hernández', correo: 'carlos.hernandez@example.com', nrc: '34567', encargado: 'Pedro Sánchez', telefono: '+503 9876-5432', monto: 3000, interes: '4%' },
+      { cliente: 'Ana González', correo: 'ana.gonzalez@example.com', nrc: '45678', encargado: 'Carmen Ruiz', telefono: '+503 3456-7890', monto: 4000, interes: '6%' },
+      { cliente: 'Luis Martínez', correo: 'luis.martinez@example.com', nrc: '56789', encargado: 'Raúl Pérez', telefono: '+503 4321-0987', monto: 5000, interes: '7%' },
+      { cliente: 'Pedro Gutiérrez', correo: 'pedro.gutierrez@example.com', nrc: '67890', encargado: 'Laura Torres', telefono: '+503 7654-3210', monto: 6000, interes: '2%' },
+      { cliente: 'Laura Torres', correo: 'laura.torres@example.com', nrc: '78901', encargado: 'Carlos Ramírez', telefono: '+503 8765-4321', monto: 7000, interes: '8%' },
+      { cliente: 'Raúl Ramírez', correo: 'raul.ramirez@example.com', nrc: '89012', encargado: 'Sofía Castro', telefono: '+503 6543-2109', monto: 8000, interes: '9%' },
+      { cliente: 'Sofía Castro', correo: 'sofia.castro@example.com', nrc: '90123', encargado: 'María León', telefono: '+503 5432-1098', monto: 9000, interes: '1%' },
+      { cliente: 'María León', correo: 'maria.leon@example.com', nrc: '01234', encargado: 'Jorge Fernández', telefono: '+503 2109-8765', monto: 10000, interes: '5%' },
+      { cliente: 'Ricardo Álvarez', correo: 'ricardo.alvarez@example.com', nrc: '11234', encargado: 'Ana Morales', telefono: '+503 5678-2345', monto: 11000, interes: '6%' },
+      { cliente: 'Ana Morales', correo: 'ana.morales@example.com', nrc: '22345', encargado: 'Luis Torres', telefono: '+503 8765-1234', monto: 12000, interes: '7%' },
+      { cliente: 'Luis Torres', correo: 'luis.torres@example.com', nrc: '33456', encargado: 'Carmen Gutiérrez', telefono: '+503 5432-6789', monto: 13000, interes: '4%' },
+      { cliente: 'Carmen Gutiérrez', correo: 'carmen.gutierrez@example.com', nrc: '44567', encargado: 'Juan Ramírez', telefono: '+503 2109-5432', monto: 14000, interes: '3%' },
+      { cliente: 'Juan Pérez', correo: 'juan.perez@example.com', nrc: '12345', encargado: 'Ana López', telefono: '+503 1234-5678', monto: 1000, interes: '5%' },
+      { cliente: 'María López', correo: 'maria.lopez@example.com', nrc: '23456', encargado: 'Luis Gómez', telefono: '+503 5678-1234', monto: 2000, interes: '3%' },
+      { cliente: 'Carlos Hernández', correo: 'carlos.hernandez@example.com', nrc: '34567', encargado: 'Pedro Sánchez', telefono: '+503 9876-5432', monto: 3000, interes: '4%' },
+      { cliente: 'Ana González', correo: 'ana.gonzalez@example.com', nrc: '45678', encargado: 'Carmen Ruiz', telefono: '+503 3456-7890', monto: 4000, interes: '6%' },
+      { cliente: 'Luis Martínez', correo: 'luis.martinez@example.com', nrc: '56789', encargado: 'Raúl Pérez', telefono: '+503 4321-0987', monto: 5000, interes: '7%' },
+      { cliente: 'Pedro Gutiérrez', correo: 'pedro.gutierrez@example.com', nrc: '67890', encargado: 'Laura Torres', telefono: '+503 7654-3210', monto: 6000, interes: '2%' },
+      { cliente: 'Laura Torres', correo: 'laura.torres@example.com', nrc: '78901', encargado: 'Carlos Ramírez', telefono: '+503 8765-4321', monto: 7000, interes: '8%' },
+      { cliente: 'Raúl Ramírez', correo: 'raul.ramirez@example.com', nrc: '89012', encargado: 'Sofía Castro', telefono: '+503 6543-2109', monto: 8000, interes: '9%' },
+      { cliente: 'Sofía Castro', correo: 'sofia.castro@example.com', nrc: '90123', encargado: 'María León', telefono: '+503 5432-1098', monto: 9000, interes: '1%' },
+      { cliente: 'María León', correo: 'maria.leon@example.com', nrc: '01234', encargado: 'Jorge Fernández', telefono: '+503 2109-8765', monto: 10000, interes: '5%' },
+      { cliente: 'Ricardo Álvarez', correo: 'ricardo.alvarez@example.com', nrc: '11234', encargado: 'Ana Morales', telefono: '+503 5678-2345', monto: 11000, interes: '6%' },
+      { cliente: 'Ana Morales', correo: 'ana.morales@example.com', nrc: '22345', encargado: 'Luis Torres', telefono: '+503 8765-1234', monto: 12000, interes: '7%' },
+      { cliente: 'Luis Torres', correo: 'luis.torres@example.com', nrc: '33456', encargado: 'Carmen Gutiérrez', telefono: '+503 5432-6789', monto: 13000, interes: '4%' },
+      { cliente: 'Carmen Gutiérrez', correo: 'carmen.gutierrez@example.com', nrc: '44567', encargado: 'Juan Ramírez', telefono: '+503 2109-5432', monto: 14000, interes: '3%' },
+      // 15 registros adicionales hasta completar 30...
+    ];
   }
 
-  verSolicitud(solicitud: any) {
-    console.log('Ver solicitud', solicitud);
-  }
-
-  onPageChange(event: any) {
-    this.first = event.first;
-    this.rows = event.rows;
+  onPageChange(event: any): void {
     console.log('Cambio de página:', event);
+  }
+
+  verSolicitud(solicitud: any): void {
+    console.log('Detalle de solicitud:', solicitud);
   }
 }
